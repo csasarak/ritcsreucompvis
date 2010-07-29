dc87d83080e48c44da069580af030bdfa3573ac7
Christopher Sasarak <chris.sasarak@gmail.com>
2010-07-29 10:43:10 -0400

    Can successfully convert an image to grayscale.
    The image is saved as a TIFF, I will not try to make it save as
    a JPEG because I was having trouble with it before.

--- ConvertToMono.java
+++ ConvertToMono.java
@@ -26,10 +26,10 @@ public class ConvertToMono{
             System.exit(1);
         }
         String inputFile = args[0]; 
-        String outputFile = inputFile.substring(0, inputFile.lastIndexOf('.')) + "GR.jpg";
+        String outputFile = inputFile.substring(0, inputFile.lastIndexOf('.')) + "GR.tif";
         PlanarImage inputImg = JAI.create("fileload", inputFile);
         PlanarImage outputImg = makeGrayScale(inputImg);
-        JAI.create("filestore", outputImg, outputFile, "JPEG", null);
+        JAI.create("filestore", outputImg, outputFile, "TIFF");
     }
 
     /**
@@ -39,15 +39,18 @@ public class ConvertToMono{
      * @return PlanarImage The new grayscale image.
      */
     public static PlanarImage makeGrayScale(PlanarImage img){
-        ParameterBlock pb = new ParameterBlock();
-        pb.addSource(img);        
-        ColorModel cm = new ComponentColorModel(ColorSpace.getInstance(ColorSpace.CS_GRAY), 
-                                                new int[] {8},
-                                                false,
-                                                false,
-                                                Transparency.OPAQUE,
-                                                DataBuffer.TYPE_BYTE);
-        pb.add(cm);
-        return JAI.create("ColorConvert", pb);
+        /**
+         * This matrix is for converting the image to grayscale.
+         * There is one destination band and n source bands ( [0][1..n]  ). 
+         * The double values are what we multiply the source pixels bands by 
+         * to get their ultimate value, which are then combined into the single band 
+         * of the output. The last item in the second part of the array is a constant
+         * value that will be added to the destination band. I have set it to 0 so 
+         * it has no effect. 
+         * The values I used are from this website: http://www.johndcook.com/blog/2009/08/24/algorithms-convert-color-grayscale/
+         */
+
+        double[][] matrix = {{.21, .71, 0.07, 0.0}};
+        return JAI.create("bandcombine", img, matrix, null);
     }
 }

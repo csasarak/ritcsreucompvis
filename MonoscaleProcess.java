d001a86795990b0271dfaf08e1493ac033aa8f08
Christopher Sasarak <chris.sasarak@gmail.com>
2010-08-02 16:05:21 -0400

    Changed PTileThreshold to MaxVarianceThreshold in MonoScaleProcess

--- MonoscaleProcess.java
+++ MonoscaleProcess.java
@@ -16,7 +16,7 @@ public class MonoscaleProcess implements ImageProcess {
      */
     public RenderedImage process(RenderedImage img){
         Histogram hist = (Histogram)JAI.create("histogram", img).getProperty("histogram");
-        double[] threshold = hist.getPTileThreshold(0.5);
+        double[] threshold = hist.getMaxVarianceThreshold();
 
         return JAI.create("binarize", img, new Double(threshold[0]));
     }

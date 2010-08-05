074a64fa9c1ef71ad91ad835eb1d184d7d1638aa
Christopher Sasarak <chris.sasarak@gmail.com>
2010-08-05 14:22:28 -0400

    Added SobelEdgeDetection to ImageProcessor

--- ImageProcessor.java
+++ ImageProcessor.java
@@ -18,10 +18,10 @@ public class ImageProcessor{
         
         //Build up the processor
         ProcessList processor = new ProcessList();
-        processor.add(new MorphologicalCloseProcess()); 
-        processor.add(new GrayConvertProcess());
-        processor.add(new MonoscaleProcess());
-        //processor.add(new CannyEdgeDetector());
+        //processor.add(new MorphologicalCloseProcess()); 
+        //processor.add(new GrayConvertProcess());
+        //processor.add(new MonoscaleProcess());
+        processor.add(new SobelDetectionProcess());
           
         for(int n = 0; n < args.length; n++){
             String inputFilename = args[n]; 

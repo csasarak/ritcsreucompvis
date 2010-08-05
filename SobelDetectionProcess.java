bb0af19037501054ee6538d8019cc24b730843db
Christopher Sasarak <chris.sasarak@gmail.com>
2010-08-05 09:30:18 -0400

    Created the SobelEdgeDetectionProcess class with stubs.

---
+++ SobelDetectionProcess.java
@@ -0,0 +1,24 @@
+
+/**
+ * filename: SobelDetectionProcess.java
+ *
+ * @class This class will perform a Sobel edge detection operation
+ * on an image and then return it after doing so.
+ *
+ * @author Christopher Sasarak
+ */
+import javax.media.jai.KernelJAI;
+import javax.media.jai.JAI;
+import java.awt.image.RenderedImage;
+public class SobelDetection implements ImageProcess{
+   
+   /**
+    * Return the parameter image after performing
+    * sobel edge detection.
+    *
+    * @param img The image to perform detection on.
+    * @return RenderedImage The image with detection performed.
+    */
+   public RenderedImage process(RenderedImage img){
+   }
+}

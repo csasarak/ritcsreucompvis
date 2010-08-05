b19c335ac041edc25eaf54ff10bda47aaa0370cb
Christopher Sasarak <chris.sasarak@gmail.com>
2010-08-05 09:39:14 -0400

    The algorithm basically works.
    It isn't as "smooth" as other implementations, and I would like to it to
    be. I'm going to look into different orderings of the operations in
    ImageProcessor to see if that's an improvement, and also check that I
    didn't make a mistake somewhere in my implementation.

--- SobelDetectionProcess.java
+++ SobelDetectionProcess.java
@@ -10,7 +10,7 @@
 import javax.media.jai.KernelJAI;
 import javax.media.jai.JAI;
 import java.awt.image.RenderedImage;
-public class SobelDetection implements ImageProcess{
+public class SobelDetectionProcess implements ImageProcess{
    
    /**
     * Return the parameter image after performing
@@ -20,5 +20,12 @@ public class SobelDetection implements ImageProcess{
     * @return RenderedImage The image with detection performed.
     */
    public RenderedImage process(RenderedImage img){
+        float[] hx = {-1,0,1,-2,0,2,-1,0,1};
+        float[] hy = {-1,-2,-1,0,0,0,1,2,1}; 
+        KernelJAI kernX = new KernelJAI(3,3, hx);
+        KernelJAI kernY = new KernelJAI(3,3, hy); 
+
+        img = JAI.create("convolve", img, kernY);
+        return JAI.create("convolve", img, kernX);
    }
 }

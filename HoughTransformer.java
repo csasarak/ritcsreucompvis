50967986019a3b8a508e3592d1309ce0355045d6
Christopher Sasarak <chris.sasarak@gmail.com>
2010-08-09 11:52:50 -0400

    Created a skeleton for HoughTransformer.
    I have not yet decided if I want to implement ImageProcess.
    I would need a few helper methods to extract where the circle
    is; which makes it atypical compared to the other ImageProcesses.
    However, the advantage is that it can just be added to the ProcessList
    and run through in a pipeline like the rest.

---
+++ HoughTransformer.java
@@ -0,0 +1,14 @@
+/**
+ * filename: HoughTransformer.java
+ *
+ * @class HoughTransformer This class will use the BIJ library to find
+ * a circle in the given image and then do its best
+ * to tell if there is an elliptical galaxy inside it.
+ *
+ * @author Christopher Sasarak
+ */
+import registration.HoughTransform;
+
+public class HoughTransformer{
+
+}
